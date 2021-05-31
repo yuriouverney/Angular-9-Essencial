ee3c81c1409e11913276bbf661736ad49708d7e5
User GCT <yuri.ouverney@hotmai.com>
2021-05-31 12:24:04 -0300

    Criando diretiva(1) de cor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './componentes/template/header/header.component';
+import { RedDirective } from './directives/red.directive';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './componentes/template/footer/footer.component';
@@ -17,6 +18,7 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './componentes/product/product-create/product-create.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +27,8 @@ import { ProductCreateComponent } from './componentes/product/product-create/pro
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
