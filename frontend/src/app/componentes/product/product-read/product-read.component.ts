c2b7a0d660033d8d257dd68376aa606c5d25fafb
User GCT <yuri.ouverney@hotmai.com>
2021-06-01 11:51:18 -0300

    Adicionando Coluna Ações

--- frontend/src/app/componentes/product/product-read/product-read.component.ts
+++ frontend/src/app/componentes/product/product-read/product-read.component.ts
@@ -11,7 +11,7 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit {
 
   products!: Product[]
-  displayedColumns = ['id', 'name', 'price']
+  displayedColumns = ['id', 'name', 'price', 'action']
 
   constructor(private productService: ProductService) { }
 
