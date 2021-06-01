c2058637b81b699162dbe97ea7451f5ebc68541c
User GCT <yuri.ouverney@hotmai.com>
2021-06-01 10:40:43 -0300

    Componente Obter Produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,7 +24,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './componentes/product/product-read/product-read.component'
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import { MatInputModule } from '@angular/material/input'
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    RedDirective
+    RedDirective,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
