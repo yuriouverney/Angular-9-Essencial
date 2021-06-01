9858d577fdaa9c367b94c60c2424c8eb64f1c3c5
User GCT <yuri.ouverney@hotmai.com>
2021-06-01 11:05:21 -0300

    Gerando Componente Tabela

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -25,7 +25,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
-import { ProductReadComponent } from './componentes/product/product-read/product-read.component'
+import { ProductReadComponent } from './componentes/product/product-read/product-read.component';
+import { ProductRead2Component } from './componentes/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
 
 
 @NgModule({
@@ -38,7 +42,8 @@ import { ProductReadComponent } from './componentes/product/product-read/product
     ProductCrudComponent,
     ProductCreateComponent,
     RedDirective,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -53,7 +58,10 @@ import { ProductReadComponent } from './componentes/product/product-read/product
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
